b8f5bd10f0103373909e785178bc942c711e9a87
Devanshi <devanshinand8@gmail.com>
2026-06-20 18:58:41 +0530

    Day-6 assignmnet upploaded

---
+++ Day-6/Find_power_of_x.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, n, i, result = 1;
+
+    printf("Enter base (x): ");
+    scanf("%d", &x);
+
+    printf("Enter power (n): ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        result = result * x;
+    }
+
+    printf("%d^%d = %d\n", x, n, result);
+
+    return 0;
+}
