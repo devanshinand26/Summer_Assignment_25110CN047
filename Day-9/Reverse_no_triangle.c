3c2e11ffdd6b8806f1aafecea4dbe25c2ee53cc5
Devanshi <devanshinand8@gmail.com>
2026-06-23 15:29:10 +0530

    uploaded

---
+++ Day-9/Reverse_no_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+int i,j,rows;
+
+printf("enter rows: ");
+scanf("%d", &rows);
+
+for (i = rows; i>= 1; i--) {
+    
+
+    for (j = i; j>= 1; j--) {
+        printf("%d", j);
+    } 
+       printf("\n");
+}
+return 0;
+}
