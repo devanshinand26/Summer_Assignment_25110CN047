7ea3be1558c855c8d1215c4b1f16f6cf56c836cb
Devanshi <devanshinand8@gmail.com>
2026-06-23 15:53:36 +0530

    day-1 assignment uploaded

--- Day-8/Character_triangle.c
+++ Day-8/Character_triangle.c
@@ -6,16 +6,12 @@ char ch;
 printf("enter number of rows: ");
 scanf("%d", &r);
 
-for (i = 1; i <= rows; i++) {
-
+for (i = 1; i <= r; i++) {
     ch = 'A'; 
-
     for (j = 1; j <= i; j++) {
         printf("%c", ch);
         ch++; 
     }
-    
-
        printf("\n");
 }
 
